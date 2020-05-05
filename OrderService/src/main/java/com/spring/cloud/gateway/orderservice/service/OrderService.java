c500733a82e38b48856638644d220378b3ac95eb
Ranjith-kumar03 <ranjithkumar03.1980@gmail.com>
2020-05-05 19:12:59 +0530

    checked my knowledge on hystrix

--- OrderService/src/main/java/com/spring/cloud/gateway/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/spring/cloud/gateway/orderservice/service/OrderService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.spring.cloud.gateway.orderservice.common.Payment;
 import com.spring.cloud.gateway.orderservice.common.TransactionRequest;
 import com.spring.cloud.gateway.orderservice.common.TransactionResponse;
@@ -28,7 +30,19 @@ public class OrderService {
 	private String message="";
 	
 	
-	
+	@HystrixCommand(fallbackMethod = "fallBackSaveMethod"
+			,threadPoolKey = "OrderPoolKey"
+			,threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")
+
+			}   
+	        ,commandProperties = {
+	        		@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            })
 	public TransactionResponse saveOrder(TransactionRequest request)
 	{
 		String url="http://PAYMENT-SERVICE/payment/dopayment";
@@ -44,6 +58,16 @@ public class OrderService {
 			message=paymentResponse.getPaymentStatus().equals("sucess") ? "Transaction happend Sucessfully" : "Transaction Failed or declined";
 			
 		return new TransactionResponse(order,paymentResponse.getAmount(),paymentResponse.getTransactionId(),message);
+	}
+	
+	public TransactionResponse fallBackSaveMethod(TransactionRequest request)
+		{
+		
+		Order order=new Order();
+		order.setId(0);
+		order.setName("");
+		order.setPrice(0.0);
+		return new TransactionResponse(order,0.0,"0","failure");
 	}
 
 }
